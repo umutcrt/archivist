f78b38fd7e73b130378b14e098b23bf211fce02b
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-08-16 21:40:00 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Umut Cörüt on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
